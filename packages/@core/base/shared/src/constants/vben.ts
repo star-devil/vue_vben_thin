c6cb33745fff2d18ef4afd3d7a6acdfc68c45531
star-devil <707298014@qq.com>
2025-04-15 10:02:12 +0800

    fix: 初步修改为精简代码

--- packages/@core/base/shared/src/constants/vben.ts
+++ packages/@core/base/shared/src/constants/vben.ts
@@ -1,14 +1,4 @@
 /**
- * @zh_CN GITHUB 仓库地址
- */
-export const VBEN_GITHUB_URL = 'https://github.com/vbenjs/vue-vben-admin';
-
-/**
- * @zh_CN 文档地址
- */
-export const VBEN_DOC_URL = 'https://doc.vben.pro';
-
-/**
  * @zh_CN Vben Logo
  */
 export const VBEN_LOGO_URL =
